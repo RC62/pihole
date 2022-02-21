f650da8d1a29c4ed3cb887a5ecb99b3a759b6c0e
RC62 <RC62@users.noreply.github.com>
2022-02-21 17:57:06 -0500

    update script

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 Echo
+cd /Volumes/Data/rob/Library/CloudStorage/OneDrive\-Personal/Raspberry\ Pi/GitHub/pihole
+sleep 2
 hostlist-compiler -c configuration.json -o Robs_compiled_adlist.txt
-sleep 5
+sleep 2
 git add *
-sleep 5
+sleep 2
 git commit -m "update script"
-sleep 5
+sleep 2
 git push
 Echo Done
