e27c906ba372d36157daf190b113b7ba3e2ad06d
RC62 <RC62@users.noreply.github.com>
2022-02-21 16:30:02 -0500

    update script

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ hostlist-compiler -c configuration.json -o Robs_compiled_adlist.txt
 sleep 5
 git add *
 sleep 5
-git commit -m "Commit message"
+git commit -m "update script"
 sleep 5
 git push
 Echo Done
