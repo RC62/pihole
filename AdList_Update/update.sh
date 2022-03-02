b4ee9b8efdf0a2c7d2fd36e66c9b3729727e048c
RC62 <RC62@users.noreply.github.com>
2022-03-01 20:43:59 -0500

    update script

--- AdList_Update/update.sh
+++ AdList_Update/update.sh
@@ -6,32 +6,38 @@ sleep 1
 
 # Compile adlist files
 hostlist-compiler -c configuration.json -o output.txt
-sleep 2
-hostlist-compiler -c configuration2.json -o output2.txt
-sleep 2
+sleep 1
+
+# hostlist-compiler -c configuration2.json -o output2.txt
+# sleep 1
 
 # Cleanup IPs
 sed -i '' "s/127.0.0.1//g" output.txt
-sleep 2
+sleep 1
+
 sed -i '' "s/0.0.0.0//g" output.txt
-sleep 2
+sleep 1
 
 # Cleanup comments - replace "!" with "#"
 sed -i '' "s/\!/\#/g" output.txt
-sleep 2
+sleep 1
 
 # Remove all empty lines
 
 sed '/^$/d' output.txt > ~/Library/CloudStorage/OneDrive\-Personal/Raspberry\ Pi/GitHub/pihole/robs_compiled_adlist.txt
-sleep 2
-sed '/^$/d' output2.txt > ~/Library/CloudStorage/OneDrive\-Personal/Raspberry\ Pi/GitHub/pihole/robs_compiled_adguard_list.txt
-sleep 2
+sleep 1
+
+# sed '/^$/d' output2.txt > ~/Library/CloudStorage/OneDrive\-Personal/Raspberry\ Pi/GitHub/pihole/robs_compiled_adguard_list.txt
+# sleep 1
 
 #Update Github project
 cd ~/Library/CloudStorage/OneDrive\-Personal/Raspberry\ Pi/GitHub/pihole/
-git add *
-sleep 2
-git commit -m "update script"
-sleep 2
-git push
 
+git add *
+sleep 1
+
+git commit -m "update script"
+sleep 1
+
+git push
+sleep 1
